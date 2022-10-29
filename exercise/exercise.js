62277bf37397cce09bcce83a27014052aec17128
ifigueroa065 <3059977500302@ingenieria.usac.edu.gt>
2022-10-29 13:25:11 -0600

    solution

---
+++ exercise/exercise.js
@@ -0,0 +1,16 @@
+export function solution(
+    json1 = {
+      name: "Mr. Michi",
+      food: "Pescado",
+    },
+    json2 = {
+      age: 12,
+      color: "Blanco",
+    }
+  ) {
+    return {
+      ...json1,
+      ...json2,
+    };
+  }
+  
