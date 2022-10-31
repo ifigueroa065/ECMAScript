153230cd08cd2c7d9f756bd9a261a1ede6cb0e79
ifigueroa065 <3059977500302@ingenieria.usac.edu.gt>
2022-10-31 10:22:29 -0600

    object entries

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,26 @@
+const usuario = {
+    name: "Andres",
+    email: "andres@correo.com",
+    age: 23
+}
+
+console.log(Object.entries(usuario)) 
+/* 
+[
+  [ 'name', 'Andres' ],
+  [ 'email', 'andres@correo.com' ],
+  [ 'age', 23 ]
+]  
+*/
+
+
+const usuario1 = {
+    name: "Andres",
+    email: "andres@correo.com",
+    age: 23
+}
+
+console.log(Object.keys(usuario1) )
+
+// [ 'name', 'email', 'age' ]
+
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,10 @@
+
+const usuario2 = {
+    name: 'Andres',
+    email: "andres@correo.com",
+    age: 23
+}
+
+console.log(Object.values(usuario2) )
+
+// [ 'Andres', 'andres@correo.com', 23 ]
