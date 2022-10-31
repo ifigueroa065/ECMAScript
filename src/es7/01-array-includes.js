2ca8f006b259bee493e9a5fbb2dc45ba0449736e
ifigueroa065 <3059977500302@ingenieria.usac.edu.gt>
2022-10-31 10:13:26 -0600

    array-includes

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,22 @@
+let numbers= [1,2,3,4,6,7,8];
+console.log(numbers.includes(4))
+
+//Utilizando strings
+const saludo = "Hola mundo"
+
+saludo.includes("Hola") // true
+saludo.includes("Mundo") // false
+saludo.includes(" ") // true
+saludo.includes("Hola", 1) // false
+saludo.includes("mundo", -5) // true
+
+
+// Utilizando arrays
+const frutas = ["manzana", "pera", "piña", "uva"]
+
+frutas.includes("manzana") // true
+frutas.includes("Pera") // false
+frutas.includes("sandía") // false
+frutas.includes("manzana", 1) // false
+frutas.includes("piña", -1) // false
+frutas[0].includes("man") // true
